5172551fecf9201014c7d9e0d5de527917a97d32
badjokke <badjokke@gmail.com>
2026-04-06 22:08:06 +0200

    feat: dummy lan forwarding test

--- kern/hana_kern.c
+++ kern/hana_kern.c
@@ -1,5 +1,6 @@
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
+#include <bpf/bpf_endian.h>
 #include <linux/if_ether.h>
 #include <linux/udp.h>
 #include <linux/ip.h>
@@ -11,7 +12,11 @@
 #define IP_HDR_SIZE 20
 #define UDP_PROT 0x11
 
-static  __always_inline void* data_pointer_at(void* data, void* data_end, __u64 offset, __u64 size){
+#ifndef mem 
+#define memcpy(dest, src, n) __builtin_memcpy((dest), (src), n)
+#endif
+
+static __always_inline void* data_pointer_at(void* data, void* data_end, __u64 offset, __u64 size){
 	if (data + offset + size > data_end){
 		return NULL;
 	}
@@ -44,7 +49,7 @@ __u32 pseudoheader_checksum(struct iphdr* iphdr) {
 	return sum;
 }
 
-__u16 udp_checksum(struct udphdr* udphdr, struct iphdr* iphdr, void* data_end){
+static __always_inline __u16 udp_checksum(struct udphdr* udphdr, struct iphdr* iphdr, void* data_end){
 	__u32 sum = pseudoheader_checksum(iphdr);
 	__u16* bfr = (__u16*) udphdr;
 	__u16 count = MAX_PAYLOAD_SIZE;
@@ -67,26 +72,48 @@ __u16 udp_checksum(struct udphdr* udphdr, struct iphdr* iphdr, void* data_end){
 	return (__u16) ~sum;
 }
 
-static int forward_ip_traffic(void* data, void* data_end, struct ethhdr* ether_header){
+static __always_inline __be32 ip_to_network_addr(__u8 ip[4]){
+	__be32 ip_network_order = 0;
+	for(int i = 0; i < 4; i++) {
+		ip_network_order |= (__be32)ip[i] << (i * 8);
+	}
+	return ip_network_order;
+}
+
+static __always_inline int forward_via_gateway(struct ethhdr* ether_header, struct iphdr* iphdr, struct udphdr* udp_header, void* data_end){
+	__u8 destination_ip[4] = {192, 168, 1, 107};
+	char gateway_mac[ETH_ALEN] = {0x84, 0x2f, 0x57, 0x4e, 0x34, 0xd7};
+
+	memcpy(ether_header->h_source, ether_header->h_dest, ETH_ALEN);
+	memcpy(ether_header->h_dest, gateway_mac, ETH_ALEN);
+
+	static const char fmt[] = "Forwarding udp traffic to PC. Dest_port: %d";
+	bpf_trace_printk(fmt, sizeof(fmt), bpf_ntohs(udp_header->dest));
+	iphdr->daddr = ip_to_network_addr(destination_ip);
+	iphdr->check = 0;
+	iphdr->check = ip_checksum((__u16*) iphdr, IP_HDR_SIZE);
+	udp_header->check = 0;
+	udp_header->check = udp_checksum(udp_header, iphdr, data_end);
+	return XDP_TX;
+}
+
+static __always_inline int forward_ip_traffic(void* data, void* data_end, struct ethhdr* ether_header){
 	struct iphdr* iphdr = (struct iphdr*) data_pointer_at(data, data_end, ETH_HDR_SIZE, IP_HDR_SIZE);
 	if (iphdr == NULL) {
 		return XDP_DROP;
 	}
 	if (iphdr->protocol != UDP_PROT) {
-		static const char fmt[] = "Only UDP communication is currently supported";
-		bpf_trace_printk(fmt, sizeof(fmt));
-		return XDP_DROP;
+		static const char fmt[] = "Only UDP load balancing currently supported. Prot: %d, %d";
+		bpf_trace_printk(fmt, sizeof(fmt), iphdr->protocol, UDP_PROT);
+		return XDP_PASS;
 	}
 	struct udphdr* udp_header = (struct udphdr*) data_pointer_at(data, data_end, ETH_HDR_SIZE + IP_HDR_SIZE, sizeof(struct udphdr));
 	if ( udp_header == NULL ) {
+		static const char fmt[] = "UDP hdr is null";
+		bpf_trace_printk(fmt, sizeof(fmt), iphdr->protocol, UDP_PROT);
 		return XDP_DROP;
 	}
-	
-	iphdr->check = 0;
-	iphdr->check = ip_checksum((__u16*)iphdr, IP_HDR_SIZE);
-	udp_header->check = 0;
-	udp_header->check = udp_checksum(udp_header, iphdr, data_end);
-	return XDP_PASS;
+	return forward_via_gateway(ether_header, iphdr, udp_header, data_end);
 }
 
 SEC("xdp")
@@ -95,7 +122,9 @@ int balancer(struct xdp_md* ctx){
 	void* data_end = (void*) (long) ctx->data_end;
 	struct ethhdr* ether_header = (struct ethhdr*) data_pointer_at(data, data_end, 0, ETH_HDR_SIZE);
 	if (ether_header == NULL || ether_header->h_proto != IP4_PROT_ETH_TYPE){
-		return XDP_DROP;
+		static const char fmt[] = "Only ipv4 load balancing is currently supported.";
+		bpf_trace_printk(fmt, sizeof(fmt));
+		return XDP_PASS;
 	}
 	return forward_ip_traffic(data, data_end, ether_header);
 }
